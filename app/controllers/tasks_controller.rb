aac69ad8a02625083ebbc8a17430801a036bb5ab
arnaud lecorvaisier <arnaud.lecorvaisier@wanadoo.fr>
2023-04-13 20:51:56 -0400

    ljb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,36 @@ class TasksController < ApplicationController
     def show
         @Task = Task.find(params["id"])
     end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        @task.save
+        redirect_to task_path(@task)
+    end
+
+    def edit
+        @Task = Task.find(params["id"])
+    end
+
+    def update
+        @Task = Task.find(params["id"])
+        @Task.update(task_params)
+        redirect_to task_path(@Task)
+    end
+
+    def destroy
+        @Task = Task.find(params["id"])
+        @Task.destroy
+        redirect_to tasks_path, status: :see_other
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  #read all
+  # root to: "tasks#index"
+  # #create
+  # get "new_task", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # #update
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id",  to: "tasks#update"
+  # #delete
+  # delete "tasks/:id", to: "tasks#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
 end
