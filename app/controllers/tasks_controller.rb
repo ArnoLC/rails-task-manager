4f03862396f307e6f31a7dd00b5c7493f4534090
arnaud lecorvaisier <arnaud.lecorvaisier@wanadoo.fr>
2023-04-13 21:01:17 -0400

    h

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @Tasks = Task.all
     end
 
-    def show
-        @Task = Task.find(params["id"])
+    def show        
     end
 
     def new
@@ -17,18 +18,15 @@ class TasksController < ApplicationController
         redirect_to task_path(@task)
     end
 
-    def edit
-        @Task = Task.find(params["id"])
+    def edit        
     end
 
-    def update
-        @Task = Task.find(params["id"])
+    def update        
         @Task.update(task_params)
         redirect_to task_path(@Task)
     end
 
-    def destroy
-        @Task = Task.find(params["id"])
+    def destroy    
         @Task.destroy
         redirect_to tasks_path, status: :see_other
     end
@@ -37,5 +35,9 @@ class TasksController < ApplicationController
 
     def task_params
         params.require(:task).permit(:title, :details, :completed)
+    end
+
+    def set_task
+        @Task = Task.find(params["id"])
     end
 end
